8e441dd3e5c4c6c1ecea9e453f1e1bbeb4cf11a0
Akzhigitov Anton <akzhigitov.anton@yandex.ru>
2014-11-07 17:08:22 +0300

    fixed cycle in skeleton

--- import/des/mc/core/skeleton.d
+++ import/des/mc/core/skeleton.d
@@ -237,7 +237,7 @@ private
 
         string type = arrayType.stringof;
 
-        foreach( e; Enum.min .. Enum.max )
+        foreach( e; [EnumMembers!(Skeleton.JointID)] )
         {
             string id = format("%s.%s",Enum.stringof,e);
             string name = toProperName( id );
